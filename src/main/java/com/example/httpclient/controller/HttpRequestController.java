91f475f6cc5cf1f7d3d1ec135cb2d03047c43a61
JunHwan Kim <nefertiri972@gmail.com>
2022-08-03 00:22:20 +0900

    Add WebClient

--- src/main/java/com/example/httpclient/controller/HttpRequestController.java
+++ src/main/java/com/example/httpclient/controller/HttpRequestController.java
@@ -4,6 +4,7 @@ import com.example.httpclient.controller.dto.TestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -14,8 +15,12 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -54,19 +59,25 @@ public class HttpRequestController {
     }
 
     @PostMapping("/request/webclient")
-    public String requestWebClient() {
+    public String requestWebClient(TestDto testDto) {
         log.info("HttpRequestController.requestWebClient started");
 
 
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("url", "test1");
-        params.add("content", "test content");
+        params.add("url", testDto.getUrl());
+        params.add("content", testDto.getContent());
 
         Mono<String> stringMono = webClient.post()
                 .uri(url)
                 .body(BodyInserters.fromFormData(params))
                 .retrieve()
+                .onStatus(HttpStatus::isError, clientResponse -> {
+                    log.info("statusCode={}", clientResponse.statusCode());
+                    return clientResponse.bodyToMono(String.class)
+                            .map(RuntimeException::new);
+                })
                 .bodyToMono(String.class);
+
 
         stringMono.subscribe(s -> log.info(s));
 
